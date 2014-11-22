98f1bf3d610866e52506dd2d2f4dc0780a53122e
Dan Salmon <dan.sa7mon@gmail.com>
2014-11-22 11:23:38 -0600

    Add second activity

--- GUIApp/src/com/example/guiapp/MainActivity.java
+++ GUIApp/src/com/example/guiapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.guiapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
-
+	public final static String EXTRA_MESSAGE = "com.example.GUIapp.MESSAGE";
+	
+	public void sendMessage (View view) {
+		// Use an Intent to start another activity
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		// Get the edit_message of the EditText to send to the next function
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		// Send the extra data over to the other activity.
+		intent.putExtra(EXTRA_MESSAGE, message);
+		// Start the second activity and send the intent with it
+		startActivity(intent);
+	}
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
